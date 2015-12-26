489d6c4ae877b5ecc1e771632b22b17fc52f11f9
Parth Desai <Parth Desai>
2015-12-26 17:41:06 -0500

    added help, quit, exit commands

--- turtle.cpp
+++ turtle.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <string.h>
+
 using namespace std;
 
 int main() {
  string rep;
+while(true) {
  cout << "turtle> ";
- cin >> rep;
+ cin >> rep; 
+ //cout << rep;
+ if (rep == "help") {
+   cout << "help\n";
+   cout << "new 'filepath' - creates a new file to draw on\n";
+   continue;
+ } else if (rep == "quit") {
+   cout << "bye\n";
+   break;
+ } else {
+   cout << "Error: " << rep << " Unknown command\n";
+   continue;
+ }
+
+}
+
+
  return 0;
 }
