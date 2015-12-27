c7ab81113c700036f54181e45d94d02235b4ea6d
Parth Desai <Parth Desai>
2015-12-27 16:44:58 -0500

    tried parsing didn't work so done with c++

--- turtle.cpp
+++ turtle.cpp
@@ -2,16 +2,52 @@
 #include <string>
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
+void error_message(string cmd) {
+  cout << "Error: " << cmd << " Unknown command\n";
+}
+
+std::vector<string> parse(string rep) {
+  cout << "parsing";
+  std::vector<string> command;
+  string temp = ""; 
+
+  for (std::string::iterator it = rep.begin(); it != rep.end(); ++it) {
+     if(it.compare(" ") == 0){
+       command.push_back (temp);
+       temp = "";
+     } else {
+       temp.append(it);
+     }
+  }
+  return command;
+}
+
 int main() {
  string rep;
-while(true) {
  cout << "turtle> ";
  cin >> rep; 
- //cout << rep;
- if (rep == "help") {
+ std::vector<string> cmd_vector = parse(rep);
+ for (std::vector<string>::const_iterator i = cmd_vector.begin(); i!= cmd_vector.end(); ++i) {
+   cout << *i << ' ';
+ } 
+ cout << '\n';
+
+ return 0;
+}
+
+
+
+//while(true) {
+
+
+//cout << rep;
+/*
+ if (command == "help") {
    cout << "help\n";
    cout << "new 'filepath' - creates a new file to draw on\n";
    continue;
@@ -19,12 +55,9 @@ while(true) {
    cout << "bye\n";
    break;
  } else {
-   cout << "Error: " << rep << " Unknown command\n";
-   continue;
+   error_message(rep)
  }
+*/
 
-}
+//}
 
-
- return 0;
-}
